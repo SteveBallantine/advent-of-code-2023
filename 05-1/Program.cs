63d8d5ebb666442facae09a3784ace207cefbd8c
steveb <steve.ballantine@gearset.com>
2023-12-05 12:42:46 +0000

    Day 5 part 2 - brute force version

---
+++ 05-1/Program.cs
@@ -0,0 +1,141 @@
+﻿var lines = File.ReadAllLines(@"C:\Repos\advent-of-code-2023\05-1\input.txt");
+
+var blocks = SplitIntoBlocks(lines).ToList();
+var seedGroups = GetSeeds(blocks.First().Single()).ToArray();
+var mapsBySource = GetMapsFromBlocksOfLines(blocks.Skip(1))
+    .ToDictionary(x => x.Source, x => x);
+
+Console.WriteLine(GetMinLocation(seedGroups, mapsBySource));
+
+
+long GetMinLocation(SeedGroup[] seedGroups, Dictionary<string, Map> mapsBySource)
+{
+    object locker = new object();
+    long minLocation = long.MaxValue;
+
+    Parallel.ForEach(seedGroups, new ParallelOptions { MaxDegreeOfParallelism = 30 }, group =>
+    {
+        Console.WriteLine($"Starting seed group with {group.Length} entries");
+        long lastMappingsSeed = 0;
+        Dictionary<string, long> lastMappings = new Dictionary<string, long>();
+
+        for (long seed = group.Start; seed < group.Start + group.Length; seed++)
+        {
+            State state = new State(seed, "seed");
+            while (state.Type != "location")
+            {
+                state = GetNextState(state, mapsBySource);
+            }
+
+            if (state.Type == "location")
+            {
+                if (state.Value < minLocation)
+                {
+                    lock (locker)
+                    {
+                        if (state.Value < minLocation)
+                        {
+                            minLocation = state.Value;
+                            Console.WriteLine($"Min location: {minLocation}");
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    return minLocation;
+}
+
+State GetNextState(State currentState, Dictionary<string, Map> dictionary)
+{
+    var map = dictionary[currentState.Type];
+
+    var matchingMapEntry = map.Entries.Where(x =>
+        currentState.Value >= x.SourceStart && currentState.Value <= x.SourceStart + x.Length).ToArray();
+    if (matchingMapEntry.Any())
+    {
+        var matchingEntry = matchingMapEntry.First();
+        currentState.Value += matchingEntry.TargetStart - matchingEntry.SourceStart;
+    }
+    
+    currentState.Type = map.Target;
+    
+    return currentState;
+}
+
+IEnumerable<SeedGroup> GetSeeds(string line)
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    
+    long? rangeStart = null;
+    
+    foreach (var part in parts)
+    {
+        if (long.TryParse(part, out var result))
+        {
+            if (rangeStart is null) { rangeStart = result; }
+            else
+            {
+                yield return new SeedGroup(rangeStart.Value, result);
+                rangeStart = null;
+            }
+        }
+    }
+}
+
+IEnumerable<Map> GetMapsFromBlocksOfLines(IEnumerable<string[]> blocks)
+{
+    foreach (var block in blocks)
+    {
+        var headerParts = block[0].Split(new [] { '-', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        List<MapEntry> entries = new List<MapEntry>();
+        for (int i = 1; i < block.Length; i++)
+        {
+            var entryParts = block[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var parsedEntryParts = entryParts.Select(x => long.Parse(x.Trim())).ToArray();
+            entries.Add(new MapEntry(parsedEntryParts[0], parsedEntryParts[1], parsedEntryParts[2]));
+        }
+        
+        yield return new Map(headerParts[0], headerParts[2], entries.ToArray());
+    }
+}
+
+
+IEnumerable<string[]> SplitIntoBlocks(string[] lines)
+{
+    List<string> result = new List<string>();
+    for (int i = 0; i < lines.Length; i++)
+    {
+        if (string.IsNullOrWhiteSpace(lines[i]))
+        {
+            yield return result.ToArray();
+            result.Clear();
+        }
+        else
+        {
+            result.Add(lines[i]);
+        }
+    }
+    yield return result.ToArray();
+}
+
+struct State
+{
+    public State(long value, string type)
+    {
+        Value = value;
+        Type = type;
+    }
+    
+    public long Value { get; set; }
+    public string Type { get; set; }
+    public long NextUsefulIncrement { get; set; } = long.MaxValue;
+}
+
+record SeedGroup(long Start, long Length);
+
+record Map(string Source, string Target, MapEntry[] Entries);
+
+record MapEntry (long TargetStart, long SourceStart,  long Length);
