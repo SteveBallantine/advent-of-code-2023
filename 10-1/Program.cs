d08bdbe1fea6232c215150653786c1d619345071
steveb <steve.ballantine@gearset.com>
2023-12-10 09:42:06 +0000

    Day 10 part 2 - only working for test inputs

--- 10-1/Program.cs
+++ 10-1/Program.cs
@@ -1,4 +1,7 @@
-﻿var lines = File.ReadAllLines(@"C:\Repos\advent-of-code-2023\10-1\input.txt");
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+var lines = File.ReadAllLines(@"C:\Repos\advent-of-code-2023\10-1\input.txt");
 var width = lines[0].Length;
 
 var start = FindStart();
@@ -9,6 +12,12 @@ HashSet<char> ConnectedEast = new HashSet<char> { 'S', '-', 'L', 'F' };
 HashSet<char> ConnectedWest = new HashSet<char> { 'S', '-', 'J', '7' };
 
 var locations = GetAdjacentConnections(start).ToArray();
+bool StartConnectedEast = locations.Any(l => l.X > start.X);
+bool StartConnectedWest = locations.Any(l => l.X < start.X);
+bool StartConnectedSouth = locations.Any(l => l.Y > start.Y);
+bool StartConnectedNorth = locations.Any(l => l.Y > start.Y);
+
+
 if (locations.Length > 2)
 {
     throw new Exception("Too many candidates from start");
@@ -17,14 +26,11 @@ if (locations.Length > 2)
 var locationA = locations[0];
 var locationB = locations[1];
 HashSet<Point> history = new HashSet<Point> { start };
-var count = 1;
 
 while (locationA != locationB && 
        !history.Contains(locationA) && 
        !history.Contains(locationB))
 {
-    Console.WriteLine($"Step {count} - {locationA.X}, {locationA.Y} and {locationB.X}, {locationB.Y}");
-    
     var options = GetAdjacentConnections(locationA);
     var next = options.Single(x => !history.Contains(x));
     history.Add(locationA);
@@ -34,12 +40,210 @@ while (locationA != locationB &&
     next = options.Single(x => !history.Contains(x));
     history.Add(locationB);
     locationB = next;
-    
-    count++;
+}
+history.Add(locationA);
+
+bool DoCrossingsVis = false;
+
+if (DoCrossingsVis)
+{
+    Console.BufferHeight = 180;
+    Console.SetCursorPosition(0, 0);
+    for (int y = 0; y < lines.Length; y++)
+    {
+        Console.WriteLine(lines[y]);
+    }
 }
 
+int enclosedCount = 0;
+for (int y = 0; y < lines.Length; y++)
+{
+    for (int x = 0; x < width; x++)
+    {
+        var p = new Point(x, y);
+        var enclosed = PointIsEnclosed(p);
+        if (enclosed && !DoCrossingsVis)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write('I');
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        else if (history.Contains(p) && !DoCrossingsVis)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(lines[y][x]);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        else if(!DoCrossingsVis)
+        {
+            Console.Write(lines[y][x]);
+        }
+        if (enclosed)
+        {
+            enclosedCount++;
+        }
+    }
+    if(!DoCrossingsVis) Console.WriteLine();
+}
 
-Console.WriteLine(count);
+if(!DoCrossingsVis) Console.WriteLine(enclosedCount);
+
+
+bool PointIsEnclosed(Point p)
+{
+    bool vis = false;
+    if (p.X == 69 && p.Y == 58)
+    {
+        vis = false;
+    }
+    
+    if (history.Contains(p)) return false;
+
+    if (vis)
+    {
+        Console.SetCursorPosition(p.X, p.Y);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(lines[p.Y][p.X]);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+    var crossingsWest = GetCrossings(p, 1, 0, vis);
+    var crossingsEast = GetCrossings(p, -1, 0, vis);
+    var crossingsNorth = GetCrossings(p, 0, -1, vis);
+    var crossingsSouth = GetCrossings(p, 0, 1, vis);
+
+    var enclosed = crossingsEast > 0 && crossingsNorth > 0 && crossingsSouth > 0 && crossingsWest > 0 &&
+                   (crossingsEast % 2 == 1 || crossingsSouth % 2 == 1 || crossingsWest % 2 == 1 || crossingsNorth % 2 == 1);
+
+    return enclosed;
+}
+
+int GetCrossings(Point start, int deltaX, int deltaY, bool vis)
+{
+    var crossings = 0;
+    var current = new Point(start.X + deltaX, start.Y + deltaY);
+
+    bool connectedPositive = false;
+    bool connectedNegative = false;
+    bool onPartial = false;
+    
+    while (current.X <= width &&
+           current.X >= 0 &&
+           current.Y <= lines.Length &&
+           current.Y >= 0)
+    {
+        if (history.Contains(current))
+        {
+            if (deltaX == 0)
+            {
+                
+                var thisConnectedNegative = (lines[current.Y][current.X] == 'S' && StartConnectedWest) || ConnectedWest.Contains(lines[current.Y][current.X]);
+                var thisConnectedPositive = (lines[current.Y][current.X] == 'S' && StartConnectedEast) || ConnectedEast.Contains(lines[current.Y][current.X]);
+                
+                if (thisConnectedNegative && thisConnectedPositive ||
+                    (onPartial && (connectedPositive && thisConnectedNegative) || (connectedNegative && thisConnectedPositive)))
+                {
+                    crossings++;
+                    connectedNegative = false;
+                    connectedPositive = false;
+                    onPartial = false;
+                    if (vis)
+                    {
+                        Console.SetCursorPosition(current.X, current.Y);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(lines[current.Y][current.X]);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                }
+                else if (thisConnectedNegative || thisConnectedPositive)
+                {
+                    if (onPartial && 
+                        thisConnectedNegative && connectedNegative ||
+                        thisConnectedPositive && connectedPositive)
+                    {
+                        connectedNegative = false;
+                        connectedPositive = false;
+                        onPartial = false;
+                        if (vis)
+                        {
+                            Console.SetCursorPosition(current.X, current.Y);
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.Write(lines[current.Y][current.X]);
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                    }
+                    else
+                    {
+                        connectedNegative = connectedNegative || thisConnectedNegative;
+                        connectedPositive = connectedPositive || thisConnectedPositive;
+                        onPartial = true;
+                        if (vis)
+                        {
+                            Console.SetCursorPosition(current.X, current.Y);
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(lines[current.Y][current.X]);
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                    }
+                }
+            }
+            
+            if (deltaY == 0)
+            {
+                var thisConnectedNegative = (lines[current.Y][current.X] == 'S' && StartConnectedNorth) || ConnectedNorth.Contains(lines[current.Y][current.X]);
+                var thisConnectedPositive = (lines[current.Y][current.X] == 'S' && StartConnectedSouth) || ConnectedSouth.Contains(lines[current.Y][current.X]);
+
+                if (thisConnectedNegative && thisConnectedPositive ||
+                    (onPartial && (connectedPositive && thisConnectedNegative) || (connectedNegative && thisConnectedPositive)))
+                {
+                    crossings++;
+                    connectedNegative = false;
+                    connectedPositive = false;
+                    onPartial = false;
+                    if (vis)
+                    {
+                        Console.SetCursorPosition(current.X, current.Y);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(lines[current.Y][current.X]);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                }
+                else if (thisConnectedNegative || thisConnectedPositive)
+                {
+                    if (onPartial && 
+                        thisConnectedNegative && connectedNegative ||
+                        thisConnectedPositive && connectedPositive)
+                    {
+                        connectedNegative = false;
+                        connectedPositive = false;
+                        onPartial = false;
+                        if (vis)
+                        {
+                            Console.SetCursorPosition(current.X, current.Y);
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.Write(lines[current.Y][current.X]);
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                    }
+                    else
+                    {
+                        connectedNegative = connectedNegative || thisConnectedNegative;
+                        connectedPositive = connectedPositive || thisConnectedPositive;
+                        onPartial = true;
+                        if (vis)
+                        {
+                            Console.SetCursorPosition(current.X, current.Y);
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(lines[current.Y][current.X]);
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                    }
+                }
+            }
+        }
+        current = new Point(current.X + deltaX, current.Y + deltaY);
+    }
+    return crossings;
+}
 
 
 Point FindStart()
